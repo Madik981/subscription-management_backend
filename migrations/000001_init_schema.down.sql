decb32e92e805ab5dec070c28511e99c8ca028d7
Madik981 <madi12312456@gmail.com>
2026-04-18 08:25:51 +0500

    Added migration for plans, users, billings tables

---
+++ migrations/000001_init_schema.down.sql
@@ -0,0 +1,7 @@
+DROP INDEX IF EXISTS idx_billings_plan_id;
+DROP INDEX IF EXISTS idx_billings_user_id;
+DROP INDEX IF EXISTS idx_users_plan_id;
+
+DROP TABLE IF EXISTS billings;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS plans;
